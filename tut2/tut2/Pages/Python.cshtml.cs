2b1aec8acfe91826e6f4692ac89d645a8ce7a631
Alec <amsears60441@gmail.com>
2022-07-05 11:02:38 -0600

    Created Python Page

---
+++ tut2/tut2/Pages/Python.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace tut2.Pages
+{
+    public class PythonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
